9aa6394800fa6aa4c5f78f640f02321bd06cd6d9
Alexis Oyama <alexis@leanplum.com>
2017-07-12 00:07:59 -0700

    chore(EventCallback): Added proper header and created bare bone unit test

---
+++ Example/Tests/Classes/EventCallbackManagerTest.m
@@ -0,0 +1,39 @@
+//
+//  EventCallbackManagerTest.m
+//  Leanplum-SDK
+//
+//  Created by Alexis Oyama on 7/12/17.
+//  Copyright © 2017 Leanplum. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EventCallbackManagerTest : XCTestCase
+
+@end
+
+@implementation EventCallbackManagerTest
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
