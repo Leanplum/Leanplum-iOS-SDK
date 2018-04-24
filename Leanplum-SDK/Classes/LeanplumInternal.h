4984d171b214d4e70272f553433e440faaa30e7c
Mayank Sanganeria <mayank.ot@gmail.com>
2018-04-24 14:40:34 -0700

    refactor: breaking up leanplum.m

--- Leanplum-SDK/Classes/LeanplumInternal.h
+++ Leanplum-SDK/Classes/LeanplumInternal.h
@@ -23,6 +23,7 @@
 //  under the License.
 
 #import "Leanplum.h"
+#import "LPActionContextInternal.h"
 #import "Constants.h"
 #import "LPActionManager.h"
 #import "LPJSON.h"
@@ -233,45 +234,5 @@ typedef void (^LeanplumInboxCacheUpdateBlock)(void);
 @property (readonly, strong) NSString *private_Name;
 @property (readonly, strong) id private_DefaultValue;
 @property (readonly, strong) NSString *private_Kind;
-
-@end
-
-#pragma mark - LPActionContext class
-
-@interface LPActionContext ()
-
-+ (LPActionContext *)actionContextWithName:(NSString *)name
-                                      args:(NSDictionary *)args
-                                 messageId:(NSString *)messageId;
-
-+ (LPActionContext *)actionContextWithName:(NSString *)name
-                                      args:(NSDictionary *)args
-                                 messageId:(NSString *)messageId
-                         originalMessageId:(NSString *)originalMessageId
-                                  priority:(NSNumber *)priority;
-
-@property (readonly, strong) NSString *private_Name;
-@property (readonly, strong) NSString *private_MessageId;
-@property (readonly, strong) NSString *private_OriginalMessageId;
-@property (readonly, strong) NSNumber *private_Priority;
-@property (readonly, strong) NSDictionary *private_Args;
-@property (readonly, strong) LPActionContext *private_ParentContext;
-@property (readonly) int private_ContentVersion;
-@property (readonly, strong) NSString *private_Key;
-@property (readonly) BOOL private_PreventRealtimeUpdating;
-@property (readonly) BOOL private_IsRooted;
-@property (readonly) BOOL private_IsPreview;
-@property (nonatomic, strong) LPContextualValues *contextualValues;
-
-- (NSString *)messageId;
-- (NSString *)originalMessageId;
-- (NSNumber *)priority;
-- (void)maybeDownloadFiles;
-- (id)objectNamed:(NSString *)name;
-- (void)preventRealtimeUpdating;
-- (void)setIsRooted:(BOOL)value;
-- (void)setIsPreview:(BOOL)preview;
-- (NSDictionary *)args;
-+ (void)sortByPriority:(NSMutableArray *)actionContexts;
 
 @end
