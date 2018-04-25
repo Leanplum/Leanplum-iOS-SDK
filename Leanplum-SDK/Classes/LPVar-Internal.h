1986b9463fe77dbea45e0423cef769aa4679c7f1
Mayank Sanganeria <mayank.ot@gmail.com>
2018-04-24 17:12:40 -0700

    refactor LPVar

---
+++ Leanplum-SDK/Classes/LPVar-Internal.h
@@ -0,0 +1,15 @@
+//
+//  LPVar.h
+//  Leanplum-iOS-SDK-source
+//
+//  Created by Mayank Sanganeria on 4/24/18.
+//
+
+#import "Leanplum-Internal.h"
+
+@interface LPVar ()
+
++(BOOL)printedCallbackWarning;
++(void)setPrintedCallbackWarning:(BOOL)newPrintedCallbackWarning;
+
+@end
