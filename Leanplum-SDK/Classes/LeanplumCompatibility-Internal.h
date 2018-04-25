4dc328f6b5a031dc4480ff8e6c2ec68fdea35a68
Mayank Sanganeria <mayank.ot@gmail.com>
2018-04-24 17:15:52 -0700

    refactor LeanplumCompatibility

---
+++ Leanplum-SDK/Classes/LeanplumCompatibility-Internal.h
@@ -0,0 +1,12 @@
+//
+//  LeanplumCompatibility.h
+//  Leanplum-iOS-SDK-source
+//
+//  Created by Mayank Sanganeria on 4/24/18.
+//
+
+#import "Leanplum-Internal.h"
+
+@interface LeanplumCompatibility ()
+
+@end
