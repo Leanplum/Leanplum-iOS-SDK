0d0d4edbef47d2692b180d8ec687f8f120fbd2df
Mayank Sanganeria <mayank.ot@gmail.com>
2018-05-09 15:52:25 -0700

    revert internal state change

--- Leanplum-SDK/Classes/LPInternalState.h
+++ Leanplum-SDK/Classes/LPInternalState.h
@@ -10,6 +10,26 @@
 #import "LPActionManager.h"
 
 @interface LPInternalState : NSObject
+{
+    NSMutableArray *_startBlocks, *_variablesChangedBlocks, *_interfaceChangedBlocks,
+    *_eventsChangedBlocks, *_noDownloadsBlocks, *_onceNoDownloadsBlocks;
+    NSMutableDictionary *_actionBlocks, *_actionResponders;
+    NSMutableSet *_startResponders, *_variablesChangedResponders, *_interfaceChangedResponders,
+    *_eventsChangedResponders, *_noDownloadsResponders;
+    NSUncaughtExceptionHandler *_customExceptionHandler;
+    LPRegisterDevice *_registration;
+    BOOL _calledStart, _hasStarted, _hasStartedAndRegisteredAsDeveloper, _startSuccessful,
+    _issuedStart;
+    BOOL _initializedMessageTemplates;
+    BOOL _stripViewControllerFromState;
+    BOOL _isScreenTrackingEnabled;
+    BOOL _isInterfaceEditingEnabled;
+    LPActionManager *_actionManager;
+    NSString *_deviceId;
+    NSString *_appVersion;
+    NSMutableArray *_userAttributeChanges;
+    BOOL _calledHandleNotification;
+}
 
 @property(strong, nonatomic) NSMutableArray *startBlocks, *variablesChangedBlocks,
 *interfaceChangedBlocks, *eventsChangedBlocks, *noDownloadsBlocks, *onceNoDownloadsBlocks,
--- Leanplum-SDK/Classes/LPInternalState.m
+++ Leanplum-SDK/Classes/LPInternalState.m
@@ -18,4 +18,37 @@
     return sharedLPInternalState;
 }
 
+- (id)init {
+    if (self = [super init]) {
+        _startBlocks = nil;
+        _variablesChangedBlocks = nil;
+        _interfaceChangedBlocks = nil;
+        _eventsChangedBlocks = nil;
+        _noDownloadsBlocks = nil;
+        _onceNoDownloadsBlocks = nil;
+        _actionBlocks = nil;
+        _actionResponders = nil;
+        _startResponders = nil;
+        _variablesChangedResponders = nil;
+        _interfaceChangedResponders = nil;
+        _eventsChangedResponders = nil;
+        _noDownloadsResponders = nil;
+        _customExceptionHandler = nil;
+        _registration = nil;
+        _calledStart = NO;
+        _hasStarted = NO;
+        _hasStartedAndRegisteredAsDeveloper = NO;
+        _startSuccessful = NO;
+        _initializedMessageTemplates = NO;
+        _actionManager = nil;
+        _deviceId = nil;
+        _userAttributeChanges = [NSMutableArray array];
+        _stripViewControllerFromState = NO;
+        _isScreenTrackingEnabled = NO;
+        _isInterfaceEditingEnabled = NO;
+        _calledHandleNotification = NO;
+    }
+    return self;
+}
+
 @end
