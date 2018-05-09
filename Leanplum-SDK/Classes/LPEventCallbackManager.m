84145fd107bf9495f906fe977239b7de9bca9714
Mayank Sanganeria <mayank.ot@gmail.com>
2018-05-09 15:17:03 -0700

    break apart lpcallback

--- Leanplum-SDK/Classes/LPEventCallbackManager.m
+++ Leanplum-SDK/Classes/LPEventCallbackManager.m
@@ -24,56 +24,8 @@
 
 #import "LPEventCallbackManager.h"
 #import "LeanplumRequest.h"
+#import "LPEventCallback.h"
 #import "LPResponse.h"
-
-@implementation LPEventCallback
-
-- (id)initWithResponseBlock:(LPNetworkResponseBlock)responseBlock
-                 errorBlock:(LPNetworkErrorBlock)errorBlock
-{
-    if (self = [super init]) {
-        self.responseBlock = [responseBlock copy];
-        self.errorBlock = [errorBlock copy];
-    }
-    return self;
-}
-
-- (void)invokeResponseWithOperation:(id<LPNetworkOperationProtocol>)operation
-                           response:(id)response
-{
-    if (!self.responseBlock) {
-        return;
-    }
-    
-    // Ensure all callbacks are on main thread.
-    if (![NSThread isMainThread]) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            self.responseBlock(operation, response);
-        });
-        return;
-    }
-
-    self.responseBlock(operation, response);
-}
-
-- (void)invokeError:(NSError *)error
-{
-    if (!self.errorBlock) {
-        return;
-    }
-    
-    // Ensure all callbacks are on main thread.
-    if (![NSThread isMainThread]) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            self.errorBlock(error);
-        });
-        return;
-    }
-    
-    self.errorBlock(error);
-}
-
-@end
 
 @implementation LPEventCallbackManager
 
