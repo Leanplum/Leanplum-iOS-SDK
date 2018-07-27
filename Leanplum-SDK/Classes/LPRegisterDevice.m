6b65eb2eae1070e8b859de2ea2c74728031c6c51
Mayank Sanganeria <mayank.ot@gmail.com>
2018-07-27 15:22:07 -0700

    lpinbox refactor

--- Leanplum-SDK/Classes/LPRegisterDevice.m
+++ Leanplum-SDK/Classes/LPRegisterDevice.m
@@ -26,6 +26,10 @@
 #import "LeanplumRequest.h"
 #import "LPResponse.h"
 #import "Constants.h"
+#import "LPRequest.h"
+#import "LPRequestManager.h"
+#import "LPFeatureFlagManager.h"
+
 
 @interface LPRegisterDevice()
 
@@ -51,22 +55,41 @@
 
 - (void)registerDevice:(NSString *)email
 {
-    LeanplumRequest *request = [LeanplumRequest post:LP_METHOD_REGISTER_FOR_DEVELOPMENT
-                                              params:@{ LP_PARAM_EMAIL: email }];
-    [request onResponse:^(id<LPNetworkOperationProtocol> operation, NSDictionary *response) {
-        LP_TRY
-        BOOL isSuccess = [LPResponse isResponseSuccess:response];
-        if (isSuccess) {
-            self.callback(YES);
-        } else {
-            [self showError:[LPResponse getResponseError:response]];
-        }
-        LP_END_TRY
-    }];
-    [request onError:^(NSError *error) {
-        [self showError:[error localizedDescription]];
-    }];
-    [request sendNow];
+    if ([[LPFeatureFlagManager sharedManager] isFeatureFlagEnabled:LP_FEATURE_FLAG_REQUEST_REFACTOR]) {
+        LPRequest *request = [LPRequest post:LP_METHOD_REGISTER_FOR_DEVELOPMENT
+                                      params:@{ LP_PARAM_EMAIL: email }];
+        [request onResponse:^(id<LPNetworkOperationProtocol> operation, NSDictionary *response) {
+            LP_TRY
+            BOOL isSuccess = [LPResponse isResponseSuccess:response];
+            if (isSuccess) {
+                self.callback(YES);
+            } else {
+                [self showError:[LPResponse getResponseError:response]];
+            }
+            LP_END_TRY
+        }];
+        [request onError:^(NSError *error) {
+            [self showError:[error localizedDescription]];
+        }];
+        [[LPRequestManager sharedManager] sendNowRequest:request];
+    } else {
+        LeanplumRequest *request = [LeanplumRequest post:LP_METHOD_REGISTER_FOR_DEVELOPMENT
+                                                  params:@{ LP_PARAM_EMAIL: email }];
+        [request onResponse:^(id<LPNetworkOperationProtocol> operation, NSDictionary *response) {
+            LP_TRY
+            BOOL isSuccess = [LPResponse isResponseSuccess:response];
+            if (isSuccess) {
+                self.callback(YES);
+            } else {
+                [self showError:[LPResponse getResponseError:response]];
+            }
+            LP_END_TRY
+        }];
+        [request onError:^(NSError *error) {
+            [self showError:[error localizedDescription]];
+        }];
+        [request sendNow];
+    }
 }
 
 @end
