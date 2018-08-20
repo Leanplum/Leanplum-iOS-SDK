291d63c46f9bd90ccc2839b5344b39381809f847
Mayank Sanganeria <mayank.ot@gmail.com>
2018-08-20 14:36:42 -0700

    Create LPRequest, LPRequestManager and LPFileUploadDownloadManager in lieu of LeanplumRequest

--- Leanplum-SDK/Classes/Managers/Networking/LPRequest.h
+++ Leanplum-SDK/Classes/Managers/Networking/LPRequest.h
@@ -1,9 +1,9 @@
 //
-//  LeanplumRequest.h
+//  LPRequest.h
 //  Leanplum
 //
-//  Created by Andrew First on 4/30/12.
-//  Copyright (c) 2012 Leanplum, Inc. All rights reserved.
+//  Created by Mayank Sanganeria on 6/30/18.
+//  Copyright (c) 2018 Leanplum, Inc. All rights reserved.
 //
 //  Licensed to the Apache Software Foundation (ASF) under one
 //  or more contributor license agreements.  See the NOTICE file
@@ -26,64 +26,18 @@
 #import "Leanplum.h"
 #import "LPNetworkFactory.h"
 
-@interface LPRequest : NSObject {
-@private
-    NSString *_httpMethod;
-    NSString *_apiMethod;
-    NSDictionary *_params;
-    LPNetworkResponseBlock _response;
-    LPNetworkErrorBlock _error;
-    BOOL _sent;
-}
+@interface LPRequest : NSObject
 
-+ (void)setAppId:(NSString *)appId withAccessKey:(NSString *)accessKey;
-+ (void)setDeviceId:(NSString *)deviceId;
-+ (void)setUserId:(NSString *)userId;
-+ (void)setUploadUrl:(NSString *)url;
+@property (nonatomic, strong) NSString *apiMethod;
+@property (nonatomic, strong) NSDictionary *params;
+@property (atomic) BOOL sent;
+@property (nonatomic, copy) LPNetworkResponseBlock responseBlock;
+@property (nonatomic, copy) LPNetworkErrorBlock errorBlock;
 
-+ (NSString *)deviceId;
-+ (NSString *)userId;
-+ (void)setToken:(NSString *)token;
-+ (void)loadToken;
-+ (void)saveToken;
-
-+ (NSString *)appId;
-+ (NSString *)token;
-
-- (void)attachApiKeys:(NSMutableDictionary *)dict;
-
-- (id)initWithHttpMethod:(NSString *)httpMethod apiMethod:(NSString *)apiMethod
-    params:(NSDictionary *)params;
-
-+ (LeanplumRequest *)get:(NSString *)apiMethod params:(NSDictionary *)params;
-+ (LeanplumRequest *)post:(NSString *)apiMethod params:(NSDictionary *)params;
++ (LPRequest *)get:(NSString *)apiMethod params:(NSDictionary *)params;
++ (LPRequest *)post:(NSString *)apiMethod params:(NSDictionary *)params;
 
 - (void)onResponse:(LPNetworkResponseBlock)response;
 - (void)onError:(LPNetworkErrorBlock)error;
-
-- (void)send;
-- (void)sendNow;
-- (void)sendEventually;
-- (void)sendIfConnected;
-- (void)sendIfConnectedSync:(BOOL)sync;
-// Sends the request if another request hasn't been sent within a particular time delay.
-- (void)sendIfDelayed;
-- (void)sendFilesNow:(NSArray *)filenames;
-
-/**
- * Sends one data. Uses sendDatasNow: internally. See this method for more information.
- */
-- (void)sendDataNow:(NSData *)data forKey:(NSString *)key;
-
-/**
- * Send datas where key is the name and object is the data.
- * For example, key can be "file0" and object is NSData of png.
- */
-- (void)sendDatasNow:(NSDictionary *)datas;
-
-- (void)downloadFile:(NSString *)path;
-
-+ (int)numPendingDownloads;
-+ (void)onNoPendingDownloads:(LeanplumVariablesChangedBlock)block;
 
 @end
