836e4bb771890c895050bd29a3654c60b516aee3
Mayank Sanganeria <mayank.ot@gmail.com>
2018-08-24 13:43:02 -0700

    add factory

---
+++ Leanplum-SDK/Classes/Managers/Networking/LPRequestFactory.m
@@ -0,0 +1,16 @@
+//
+//  LPRequestFactory.h
+//  Leanplum-iOS-SDK-source
+//
+//  Created by Mayank Sanganeria on 8/23/18.
+//
+
+#import <Foundation/Foundation.h>
+#import "LPRequesting.h"
+
+@interface LPRequestFactory : NSObject
+
++ (id<LPRequesting>)get:(NSString *)apiMethod params:(NSDictionary *)params;
++ (id<LPRequesting>)post:(NSString *)apiMethod params:(NSDictionary *)params;
+
+@end
