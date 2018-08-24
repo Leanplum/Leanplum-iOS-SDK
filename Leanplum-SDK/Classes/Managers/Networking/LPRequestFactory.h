fd0389b4145bec336a95d4355b436788161d626f
Mayank Sanganeria <mayank.ot@gmail.com>
2018-08-24 10:36:53 -0700

    add request factory

---
+++ Leanplum-SDK/Classes/Managers/Networking/LPRequestFactory.h
@@ -0,0 +1,27 @@
+//
+//  LPRequestFactory.m
+//  Leanplum-iOS-SDK-source
+//
+//  Created by Mayank Sanganeria on 8/23/18.
+//
+
+#import "LPRequestFactory.h"
+#import "LPRequest.h"
+#import "LeanplumRequest.h"
+#import "LeanplumInternal.h"
+
+@implementation LPRequestFactory
+
++ (id<LPRequesting>)get:(NSString *)apiMethod params:(NSDictionary *)params {
+    LPLogType level = [apiMethod isEqualToString:LP_METHOD_LOG] ? LPDebug : LPVerbose;
+    LPLog(level, @"Will call API method %@ with arguments %@", apiMethod, params);
+    return [[LeanplumRequest alloc] initWithHttpMethod:@"GET" apiMethod:apiMethod params:params];
+}
+
++ (id<LPRequesting>)post:(NSString *)apiMethod params:(NSDictionary *)params {
+    LPLogType level = [apiMethod isEqualToString:LP_METHOD_LOG] ? LPDebug : LPVerbose;
+    LPLog(level, @"Will call API method %@ with arguments %@", apiMethod, params);
+    return [[LeanplumRequest alloc] initWithHttpMethod:@"POST" apiMethod:apiMethod params:params];
+}
+
+@end
