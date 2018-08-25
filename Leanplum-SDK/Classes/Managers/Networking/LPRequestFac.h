455631ecb602a12a5e0720fea9f7bd88a5332952
Mayank Sanganeria <mayank.ot@gmail.com>
2018-08-24 17:28:43 -0700

    add requestiung

--- Leanplum-SDK/Classes/Managers/Networking/LPRequestFac.h
+++ Leanplum-SDK/Classes/Managers/Networking/LPRequestFac.h
@@ -1,5 +1,9 @@
 #import <Foundation/Foundation.h>
+#import "LPRequesting.h"
 
 @interface LPRequestFac : NSObject
+
++ (id<LPRequesting>)get:(NSString *)apiMethod params:(NSDictionary *)params;
++ (id<LPRequesting>)post:(NSString *)apiMethod params:(NSDictionary *)params;
 
 @end
--- Leanplum-SDK/Classes/Managers/Networking/LPRequestFac.m
+++ Leanplum-SDK/Classes/Managers/Networking/LPRequestFac.m
@@ -1,7 +1,19 @@
 
 #import "LPRequestFac.h"
+#import "LeanplumRequest.h"
 
 @implementation LPRequestFac
 
++ (id<LPRequesting>)get:(NSString *)apiMethod params:(NSDictionary *)params {
+//    LPLogType level = [apiMethod isEqualToString:LP_METHOD_LOG] ? LPDebug : LPVerbose;
+//    LPLog(level, @"Will call API method %@ with arguments %@", apiMethod, params);
+    return [[LeanplumRequest alloc] initWithHttpMethod:@"GET" apiMethod:apiMethod params:params];
+}
+
++ (id<LPRequesting>)post:(NSString *)apiMethod params:(NSDictionary *)params {
+//    LPLogType level = [apiMethod isEqualToString:LP_METHOD_LOG] ? LPDebug : LPVerbose;
+//    LPLog(level, @"Will call API method %@ with arguments %@", apiMethod, params);
+    return [[LeanplumRequest alloc] initWithHttpMethod:@"POST" apiMethod:apiMethod params:params];
+}
 
 @end
