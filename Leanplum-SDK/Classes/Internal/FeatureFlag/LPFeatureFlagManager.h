c1486b42b8817fdefc92941cd4d146569cbee6b4
Mayank Sanganeria <mayank.ot@gmail.com>
2018-08-25 19:42:49 -0700

    add switch to new request on feature flag

--- Leanplum-SDK/Classes/Internal/FeatureFlag/LPFeatureFlagManager.h
+++ Leanplum-SDK/Classes/Internal/FeatureFlag/LPFeatureFlagManager.h
@@ -6,6 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LPFeatureFlags.h"
 
 @interface LPFeatureFlagManager : NSObject
 
---
+++ Leanplum-SDK/Classes/Internal/FeatureFlag/LPFeatureFlags.h
@@ -0,0 +1,13 @@
+//
+//  LPFeatureFlags.h
+//  Pods
+//
+//  Created by Mayank Sanganeria on 8/25/18.
+//
+
+#ifndef LPFeatureFlags_h
+#define LPFeatureFlags_h
+
+#define LP_FEATURE_FLAG_REQUEST_REFACTOR @"LP_FEATURE_FLAG_REQUEST_REFACTOR"
+
+#endif /* LPFeatureFlags_h */
