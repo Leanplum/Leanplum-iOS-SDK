fde7e7072a6bca376ca54804443c19cee46e171f
Grace Gu <gg4race@gmail.com>
2018-09-12 16:21:26 -0700

    add counting ios client (#198)
    * add counting ios client
    
    * id key -> NSString, remove hardcoded strings
    
    * stackoverflow fix
    
    * rename Manager->Aggregator, other minor changes
    
    * rename incrementBy
    
    * added some unit tests
    
    * consistent test methods, nullability

---
+++ Example/Tests/Classes/LPCountAggregatorTest.m
@@ -0,0 +1,99 @@
+//
+//  AppIconManagerTest.m
+//  Leanplum-SDK
+//
+//  Created by Alexis Oyama on 3/2/17.
+//  Copyright © 2017 Leanplum. All rights reserved.
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+
+
+#import <XCTest/XCTest.h>
+#import "LPCountAggregator.h"
+
+/**
+ * Expose private class methods
+ */
+@interface LPCountAggregator(UnitTest)
+
+@property (nonatomic, strong) NSMutableDictionary *counts;
+
+@end
+
+@interface LPCountAggregatorTest : XCTestCase
+
+@end
+
+@implementation LPCountAggregatorTest
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)test_incrementDisabledCount {
+    LPCountAggregator *countAggregator = [[LPCountAggregator alloc] init];
+    NSString *testString = @"test";
+
+    [countAggregator incrementCount:testString];
+    XCTAssert([countAggregator.counts[testString] intValue] == 0);
+    
+    [countAggregator incrementCount:testString];
+    XCTAssert([countAggregator.counts[testString] intValue] == 0);
+}
+
+- (void)test_incrementCount {
+    LPCountAggregator *countAggregator = [[LPCountAggregator alloc] init];
+    NSString *testString = @"test";
+    countAggregator.enabledCounters = [NSSet setWithObjects:testString, nil];
+    
+    [countAggregator incrementCount:testString];
+    XCTAssert([countAggregator.counts[testString] intValue] == 1);
+    
+    [countAggregator incrementCount:testString];
+    XCTAssert([countAggregator.counts[testString] intValue] == 2);
+    
+}
+
+- (void)test_incrementDisabledCountMultiple {
+    LPCountAggregator *countAggregator = [[LPCountAggregator alloc] init];
+    NSString *testString = @"test";
+    
+    [countAggregator incrementCount:testString by:2];
+    XCTAssert([countAggregator.counts[testString] intValue] == 0);
+    
+    [countAggregator incrementCount:testString by:15];
+    XCTAssert([countAggregator.counts[testString] intValue] == 0);
+}
+
+- (void)test_incrementCountMultiple {
+    LPCountAggregator *countAggregator = [[LPCountAggregator alloc] init];
+    NSString *testString = @"test";
+    countAggregator.enabledCounters = [NSSet setWithObjects:testString, nil];
+    
+    [countAggregator incrementCount:testString by:2];
+    XCTAssert([countAggregator.counts[testString] intValue] == 2);
+    
+    [countAggregator incrementCount:testString by:15];
+    XCTAssert([countAggregator.counts[testString] intValue] == 17);
+}
+
+@end
--- Leanplum-SDK/Classes/Internal/Leanplum.m
+++ Leanplum-SDK/Classes/Internal/Leanplum.m
@@ -45,6 +45,7 @@
 #import "Utils.h"
 #import "LPAppIconManager.h"
 #import "LPUIEditorWrapper.h"
+#import "LPCountAggregator.h"
 #import "LPRequestFactory.h"
 #import "LPRequestSender.h"
 #import "LPAPIConfig.h"
@@ -878,6 +879,8 @@ BOOL inForeground = NO;
         NSDictionary *regions = response[LP_KEY_REGIONS];
         NSDictionary *variantDebugInfo = [self parseVariantDebugInfoFromResponse:response];
         [[LPVarCache sharedCache] setVariantDebugInfo:variantDebugInfo];
+        NSSet *enabledCounters = [self parseEnabledCountersFromResponse:response];
+        [LPCountAggregator sharedAggregator].enabledCounters = enabledCounters;
 
         [[LPAPIConfig sharedConfig] setToken:token];
         [[LPAPIConfig sharedConfig] saveToken];
@@ -2656,10 +2659,18 @@ void LPLog(LPLogType type, NSString *format, ...) {
     LP_END_TRY
 }
 
-+(NSDictionary *)parseVariantDebugInfoFromResponse:(NSDictionary *)response
++ (NSDictionary *)parseVariantDebugInfoFromResponse:(NSDictionary *)response
 {
     if ([response objectForKey:LP_KEY_VARIANT_DEBUG_INFO]) {
         return response[LP_KEY_VARIANT_DEBUG_INFO];
+    }
+    return nil;
+}
+
++ (NSSet *)parseEnabledCountersFromResponse:(NSDictionary *)response
+{
+    if ([response objectForKey:LP_KEY_ENABLED_COUNTERS]) {
+        return [NSSet setWithArray:response[LP_KEY_ENABLED_COUNTERS]];
     }
     return nil;
 }
