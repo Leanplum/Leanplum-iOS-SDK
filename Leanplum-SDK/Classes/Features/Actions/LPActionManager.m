2889afe67d2519cfec7e54634938a076ba7377f5
hrishi <hrishikesh.amravatkar@leanplum.com>
2019-07-26 09:31:47 -0700

    LP-10840: Crash for localnotification fix

--- Leanplum-SDK/Classes/Features/Actions/LPActionManager.m
+++ Leanplum-SDK/Classes/Features/Actions/LPActionManager.m
@@ -197,8 +197,8 @@ API_AVAILABLE(ios(10.0)) API_AVAILABLE(ios(10.0)){
                                            fetchCompletionHandler:nil];
     LP_END_TRY
 
-    // Call overridden method.
-    if ([self respondsToSelector:@selector(leanplum_application:didReceiveLocalNotification:)]) {
+    if (swizzledApplicationDidReceiveLocalNotification &&
+        [self respondsToSelector:@selector(leanplum_application:didReceiveLocalNotification:)]) {
         [self performSelector:@selector(leanplum_application:didReceiveLocalNotification:)
                    withObject:application withObject:localNotification];
     }
