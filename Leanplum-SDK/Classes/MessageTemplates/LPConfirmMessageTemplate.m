4580a1475ceabd5b7d44d76ce65ebc0d990be44d
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-01-27 15:00:55 -0800

    add confirmation style

--- Leanplum-SDK/Classes/MessageTemplates/LPConfirmMessageTemplate.m
+++ Leanplum-SDK/Classes/MessageTemplates/LPConfirmMessageTemplate.m
@@ -1,47 +1,53 @@
 //
-//  LPAlertMessageTemplate.m
+//  LPConfirmMessageTemplate.m
 //  LeanplumSDK-iOS
 //
 //  Created by Mayank Sanganeria on 1/27/20.
 //  Copyright © 2020 Leanplum. All rights reserved.
 //
 
-#import "LPAlertMessageTemplate.h"
+#import "LPConfirmMessageTemplate.h"
 #import "Leanplum.h"
 
-@implementation LPAlertMessageTemplate
+@implementation LPConfirmMessageTemplate
 
 -(void)defineActionWithContexts:(NSMutableArray *)contexts {
     [super defineActionWithContexts:contexts];
-    [Leanplum defineAction:LPMT_ALERT_NAME
+    [Leanplum defineAction:LPMT_CONFIRM_NAME
                     ofKind:kLeanplumActionKindMessage | kLeanplumActionKindAction
              withArguments:@[
                              [LPActionArg argNamed:LPMT_ARG_TITLE withString:APP_NAME],
-                             [LPActionArg argNamed:LPMT_ARG_MESSAGE withString:LPMT_DEFAULT_ALERT_MESSAGE],
-                             [LPActionArg argNamed:LPMT_ARG_DISMISS_TEXT withString:LPMT_DEFAULT_OK_BUTTON_TEXT],
-                             [LPActionArg argNamed:LPMT_ARG_DISMISS_ACTION withAction:nil]
+                             [LPActionArg argNamed:LPMT_ARG_MESSAGE withString:LPMT_DEFAULT_CONFIRM_MESSAGE],
+                             [LPActionArg argNamed:LPMT_ARG_ACCEPT_TEXT withString:LPMT_DEFAULT_YES_BUTTON_TEXT],
+                             [LPActionArg argNamed:LPMT_ARG_CANCEL_TEXT withString:LPMT_DEFAULT_NO_BUTTON_TEXT],
+                             [LPActionArg argNamed:LPMT_ARG_ACCEPT_ACTION withAction:nil],
+                             [LPActionArg argNamed:LPMT_ARG_CANCEL_ACTION withAction:nil],
                              ]
              withResponder:^BOOL(LPActionContext *context) {
                  @try {
                      if (NSClassFromString(@"UIAlertController")) {
                          UIAlertController *alert = [UIAlertController alertControllerWithTitle:NSLocalizedString([context stringNamed:LPMT_ARG_TITLE], nil) message:NSLocalizedString([context stringNamed:LPMT_ARG_MESSAGE], nil) preferredStyle:UIAlertControllerStyleAlert];
-                         UIAlertAction *action = [UIAlertAction actionWithTitle:NSLocalizedString([context stringNamed:LPMT_ARG_DISMISS_TEXT], nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+                         UIAlertAction *cancel = [UIAlertAction actionWithTitle:NSLocalizedString([context stringNamed:LPMT_ARG_CANCEL_TEXT], nil) style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
                              [self alertDismissedWithButtonIndex:0];
                          }];
-                         [alert addAction:action];
+                         [alert addAction:cancel];
+                         UIAlertAction *accept = [UIAlertAction actionWithTitle:NSLocalizedString([context stringNamed:LPMT_ARG_ACCEPT_TEXT], nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+                             [self alertDismissedWithButtonIndex:1];
+                         }];
+                         [alert addAction:accept];
 
-                         [[self visibleViewController] presentViewController:alert animated:YES completion:nil];
+                         [[self visibleViewController]
+                          presentViewController:alert animated:YES completion:nil];
                      } else
                      {
                          UIAlertView *alert = [[UIAlertView alloc]
                                                initWithTitle:NSLocalizedString([context stringNamed:LPMT_ARG_TITLE], nil)
                                                message:NSLocalizedString([context stringNamed:LPMT_ARG_MESSAGE], nil)
                                                delegate:self
-                                               cancelButtonTitle:NSLocalizedString([context stringNamed:LPMT_ARG_DISMISS_TEXT], nil)
-                                               otherButtonTitles:nil];
+                                               cancelButtonTitle:NSLocalizedString([context stringNamed:LPMT_ARG_CANCEL_TEXT], nil)
+                                               otherButtonTitles:NSLocalizedString([context stringNamed:LPMT_ARG_ACCEPT_TEXT], nil),nil];
                          [alert show];
                      }
-
                      [self.contexts addObject:context];
                      return YES;
                  }
@@ -50,7 +56,7 @@
                      return NO;
                  }
              }];
-
+    
 }
 
 - (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
