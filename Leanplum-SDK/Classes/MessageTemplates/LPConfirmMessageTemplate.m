88c1248180cc8199bddf95dba9dd506890dac4f0
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-01-27 15:03:10 -0800

    refactor into base

--- Leanplum-SDK/Classes/MessageTemplates/LPConfirmMessageTemplate.m
+++ Leanplum-SDK/Classes/MessageTemplates/LPConfirmMessageTemplate.m
@@ -59,30 +59,4 @@
     
 }
 
-- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
-{
-    [self alertDismissedWithButtonIndex:buttonIndex];
-}
-
-- (void)alertDismissedWithButtonIndex:(NSInteger)buttonIndex
-{
-    LPActionContext *context = self.contexts.lastObject;
-    @try {
-        [self.contexts removeLastObject];
-
-        if ([context.actionName isEqualToString:LPMT_ALERT_NAME]) {
-            [context runActionNamed:LPMT_ARG_DISMISS_ACTION];
-        } else {
-            if (buttonIndex == 1) {
-                [context runTrackedActionNamed:LPMT_ARG_ACCEPT_ACTION];
-            } else {
-                [context runActionNamed:LPMT_ARG_CANCEL_ACTION];
-            }
-        }
-    }
-    @catch (NSException *exception) {
-        LOG_LP_MESSAGE_EXCEPTION;
-    }
-}
-
 @end
