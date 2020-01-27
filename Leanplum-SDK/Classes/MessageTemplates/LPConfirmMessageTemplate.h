c07c4f5e95ee80246cd66aec1187728627625c1f
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-01-27 15:00:55 -0800

    add confirmation style

---
+++ Leanplum-SDK/Classes/MessageTemplates/LPConfirmMessageTemplate.h
@@ -0,0 +1,17 @@
+//
+//  LPConfirmMessageTemplate.h
+//  LeanplumSDK-iOS
+//
+//  Created by Mayank Sanganeria on 1/27/20.
+//  Copyright © 2020 Leanplum. All rights reserved.
+//
+
+#import "LPBaseMessageTemplate.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LPConfirmMessageTemplate : LPBaseMessageTemplate
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Leanplum-SDK/Classes/MessageTemplates/LPMessageTemplates.m
+++ Leanplum-SDK/Classes/MessageTemplates/LPMessageTemplates.m
@@ -31,7 +31,7 @@
 #import <StoreKit/StoreKit.h>
 #import "LPCountAggregator.h"
 #import "LPAlertMessageTemplate.h"
-#import "LPMessageTemplateConstants.h"
+#import "LPConfirmMessageTemplate.h"
 
 #define APP_NAME (([[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"]) ?: \
     ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"]))
@@ -133,50 +133,8 @@ static NSString *DEFAULTS_LEANPLUM_ENABLED_PUSH = @"__Leanplum_enabled_push";
 {
 
     [[[LPAlertMessageTemplate alloc] init] defineActionWithContexts:_contexts];
-    [Leanplum defineAction:LPMT_CONFIRM_NAME
-                    ofKind:kLeanplumActionKindMessage | kLeanplumActionKindAction
-             withArguments:@[
-                             [LPActionArg argNamed:LPMT_ARG_TITLE withString:APP_NAME],
-                             [LPActionArg argNamed:LPMT_ARG_MESSAGE withString:LPMT_DEFAULT_CONFIRM_MESSAGE],
-                             [LPActionArg argNamed:LPMT_ARG_ACCEPT_TEXT withString:LPMT_DEFAULT_YES_BUTTON_TEXT],
-                             [LPActionArg argNamed:LPMT_ARG_CANCEL_TEXT withString:LPMT_DEFAULT_NO_BUTTON_TEXT],
-                             [LPActionArg argNamed:LPMT_ARG_ACCEPT_ACTION withAction:nil],
-                             [LPActionArg argNamed:LPMT_ARG_CANCEL_ACTION withAction:nil],
-                             ]
-             withResponder:^BOOL(LPActionContext *context) {
-                 @try {
-                     if (NSClassFromString(@"UIAlertController")) {
-                         UIAlertController *alert = [UIAlertController alertControllerWithTitle:NSLocalizedString([context stringNamed:LPMT_ARG_TITLE], nil) message:NSLocalizedString([context stringNamed:LPMT_ARG_MESSAGE], nil) preferredStyle:UIAlertControllerStyleAlert];
-                         UIAlertAction *cancel = [UIAlertAction actionWithTitle:NSLocalizedString([context stringNamed:LPMT_ARG_CANCEL_TEXT], nil) style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
-                             [self alertDismissedWithButtonIndex:0];
-                         }];
-                         [alert addAction:cancel];
-                         UIAlertAction *accept = [UIAlertAction actionWithTitle:NSLocalizedString([context stringNamed:LPMT_ARG_ACCEPT_TEXT], nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-                             [self alertDismissedWithButtonIndex:1];
-                         }];
-                         [alert addAction:accept];
+    [[[LPConfirmMessageTemplate alloc] init] defineActionWithContexts:_contexts];
 
-                         [[LPMessageTemplatesClass visibleViewController]
-                          presentViewController:alert animated:YES completion:nil];
-                     } else
-                     {
-                         UIAlertView *alert = [[UIAlertView alloc]
-                                               initWithTitle:NSLocalizedString([context stringNamed:LPMT_ARG_TITLE], nil)
-                                               message:NSLocalizedString([context stringNamed:LPMT_ARG_MESSAGE], nil)
-                                               delegate:self
-                                               cancelButtonTitle:NSLocalizedString([context stringNamed:LPMT_ARG_CANCEL_TEXT], nil)
-                                               otherButtonTitles:NSLocalizedString([context stringNamed:LPMT_ARG_ACCEPT_TEXT], nil),nil];
-                         [alert show];
-                     }
-                     [self->_contexts addObject:context];
-                     return YES;
-                 }
-                 @catch (NSException *exception) {
-                     LOG_LP_MESSAGE_EXCEPTION;
-                     return NO;
-                 }
-             }];
-    
     [Leanplum defineAction:LPMT_OPEN_URL_NAME
                     ofKind:kLeanplumActionKindAction
              withArguments:@[[LPActionArg argNamed:LPMT_ARG_URL withString:LPMT_DEFAULT_URL]]
