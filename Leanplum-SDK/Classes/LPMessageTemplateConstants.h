ae667e1fe05f864df721e9b1e2de485ba51bb610
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-01-29 13:09:15 -0800

    try

--- Leanplum-SDK/Classes/LPMessageTemplateConstants.h
+++ Leanplum-SDK/Classes/LPMessageTemplateConstants.h
@@ -9,17 +9,6 @@
 #ifndef LPMessageTemplateConstants_h
 #define LPMessageTemplateConstants_h
 
-#define LPMT_ALERT_NAME @"Alert"
-#define LPMT_CONFIRM_NAME @"Confirm"
-#define LPMT_PUSH_ASK_TO_ASK @"Push Ask to Ask"
-#define LPMT_REGISTER_FOR_PUSH @"Register For Push"
-#define LPMT_CENTER_POPUP_NAME @"Center Popup"
-#define LPMT_INTERSTITIAL_NAME @"Interstitial"
-#define LPMT_WEB_INTERSTITIAL_NAME @"Web Interstitial"
-#define LPMT_OPEN_URL_NAME @"Open URL"
-#define LPMT_HTML_NAME @"HTML"
-#define LPMT_APP_RATING_NAME @"Request App Rating"
-#define LPMT_ICON_CHANGE_NAME @"Change App Icon"
 
 
 #endif /* LPMessageTemplateConstants_h */
--- Leanplum-SDK/Classes/LPMessageTemplates.m
+++ Leanplum-SDK/Classes/LPMessageTemplates.m
@@ -35,6 +35,18 @@
 #define APP_NAME (([[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"]) ?: \
     ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"]))
 
+#define LPMT_ALERT_NAME @"Alert"
+#define LPMT_CONFIRM_NAME @"Confirm"
+#define LPMT_PUSH_ASK_TO_ASK @"Push Ask to Ask"
+#define LPMT_REGISTER_FOR_PUSH @"Register For Push"
+#define LPMT_CENTER_POPUP_NAME @"Center Popup"
+#define LPMT_INTERSTITIAL_NAME @"Interstitial"
+#define LPMT_WEB_INTERSTITIAL_NAME @"Web Interstitial"
+#define LPMT_OPEN_URL_NAME @"Open URL"
+#define LPMT_HTML_NAME @"HTML"
+#define LPMT_APP_RATING_NAME @"Request App Rating"
+#define LPMT_ICON_CHANGE_NAME @"Change App Icon"
+
 #define LPMT_ARG_TITLE @"Title"
 #define LPMT_ARG_MESSAGE @"Message"
 #define LPMT_ARG_URL @"URL"
