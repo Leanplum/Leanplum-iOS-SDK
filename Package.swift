2354253b881a3e078403743236c8dcf7dde2d284
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-04-20 13:41:20 -0700

    add package file

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "Leanplum",
+    products: [
+        .library(name: "Leanplum", targets: ["Leanplum"])
+    ],
+    targets: [
+        .target(
+        	name: "Leanplum", 
+        	path: "Leanplum-SDK/Classes",
+        	publicHeadersPath: ".",
+        )
+    ]
+)
