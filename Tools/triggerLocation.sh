c4810cca0eb31c6eaeba876a8a97f8594fc09995
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-04-30 14:55:13 -0700

    update version

--- Tools/triggerLocation.sh
+++ Tools/triggerLocation.sh
@@ -9,5 +9,4 @@ cp ../sdk-version.txt .
 git add sdk-version.txt
 git commit -m "update version"
 git tag `cat sdk-version.txt`
-git push https://${GITHUB_TOKEN}@github.com/Leanplum/Leanplum-iOS-Location.git master
-git push https://${GITHUB_TOKEN}@github.com/Leanplum/Leanplum-iOS-Location.git master `cat sdk-version.txt`
+git push https://${GITHUB_TOKEN}@github.com/Leanplum/Leanplum-iOS-Location.git master --follow-tags
