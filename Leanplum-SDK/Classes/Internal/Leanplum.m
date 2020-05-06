3064785a9ffc9d5de1ddfab45eade3aa981e0e6c
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-05-05 20:39:13 -0700

    dont crash on start in dev mode

--- Leanplum-SDK/Classes/Internal/Leanplum.m
+++ Leanplum-SDK/Classes/Internal/Leanplum.m
@@ -776,7 +776,7 @@ BOOL inForeground = NO;
             [Leanplum resume];
             return;
         }
-        [self throwError:@"Already called start."];
+        NSLog(@"Leanplum: Error: Already called start.");
     }
 
     [LPMessageTemplatesClass sharedTemplates];
