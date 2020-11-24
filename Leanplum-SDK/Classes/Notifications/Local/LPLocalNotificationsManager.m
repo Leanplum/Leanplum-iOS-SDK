3379bbf16fe97c8e5089838afab6ae8b0939af44
dejan2k <dejan.krstevski@leanplum.com>
2020-11-24 11:27:30 +0100

    Using UNUserNotification instead of deprecated UILocalNotification class (#411)
    * Using UNUserNotification instead of deprecated UILocalNotification class
    
    * handle cancel action
    add semaphore for to wait for blocks to finish
    
    * change wait time to be max 5 seconds

--- Leanplum-SDK/Classes/Notifications/Local/LPLocalNotificationsManager.m
+++ Leanplum-SDK/Classes/Notifications/Local/LPLocalNotificationsManager.m
@@ -10,6 +10,7 @@
 #import "LeanplumInternal.h"
 #import "LPNotificationsConstants.h"
 #import "LPNotificationsManager.h"
+#import <UserNotifications/UNUserNotificationCenter.h>
 
 @implementation LPLocalNotificationsManager
 
@@ -41,15 +42,9 @@
         BOOL contentAvailable = [context boolNamed:@"iOS options.Preload content"];
         NSString *message = [context stringNamed:@"Message"];
 
-        // Don't send notification if the user doesn't have the permission enabled.
-        if ([app respondsToSelector:@selector(currentUserNotificationSettings)]) {
-            BOOL isSilentNotification = message.length == 0 && contentAvailable;
-            if (!isSilentNotification) {
-                UIUserNotificationSettings *currentSettings = [app currentUserNotificationSettings];
-                if ([currentSettings types] == UIUserNotificationTypeNone) {
-                    return NO;
-                }
-            }
+        if (![self shouldSendNotificationForMessage:message contentAvailable:contentAvailable])
+        {
+            return NO;
         }
 
         NSString *messageId = context.messageId;
@@ -67,53 +62,17 @@
         int countdownSeconds = [countdown intValue];
         NSDate *eta = [[NSDate date] dateByAddingTimeInterval:countdownSeconds];
 
-        // If there's already one scheduled before the eta, discard this.
-        // Otherwise, discard the scheduled one.
-        NSArray *notifications = [app scheduledLocalNotifications];
-        for (UILocalNotification *notification in notifications) {
-            NSString *messageId = [[LPNotificationsManager shared] messageIdFromUserInfo:[notification userInfo]];
-            if ([messageId isEqualToString:context.messageId]) {
-                NSComparisonResult comparison = [notification.fireDate compare:eta];
-                if (comparison == NSOrderedAscending) {
-                    return NO;
-                } else {
-                    [app cancelLocalNotification:notification];
-                }
-            }
+        if ([self shouldDiscard:eta context:context])
+        {
+            return NO;
         }
-
-        UILocalNotification *localNotif = [[UILocalNotification alloc] init];
-        localNotif.fireDate = eta;
-        localNotif.timeZone = [NSTimeZone defaultTimeZone];
-        if (message) {
-            localNotif.alertBody = message;
-        } else {
-            localNotif.alertBody = LP_VALUE_DEFAULT_PUSH_MESSAGE;
-        }
-        localNotif.alertAction = @"View";
-
-        if ([localNotif respondsToSelector:@selector(setCategory:)]) {
-            NSString *category = [context stringNamed:@"iOS options.Category"];
-            if (category) {
-                localNotif.category = category;
-            }
-        }
-
-        NSString *sound = [context stringNamed:@"iOS options.Sound"];
-        if (sound) {
-            localNotif.soundName = sound;
-        } else {
-            localNotif.soundName = UILocalNotificationDefaultSoundName;
-        }
-
-        NSString *badge = [context stringNamed:@"iOS options.Badge"];
-        if (badge) {
-            localNotif.applicationIconBadgeNumber = [badge intValue];
-        }
-
+        
         NSDictionary *userInfo = [context dictionaryNamed:@"Advanced options.Data"];
         NSString *openAction = [context stringNamed:LP_VALUE_DEFAULT_PUSH_ACTION];
         BOOL muteInsideApp = [context boolNamed:@"Advanced options.Mute inside app"];
+        NSString *sound = [context stringNamed:@"iOS options.Sound"];
+        NSString *badge = [context stringNamed:@"iOS options.Badge"];
+        NSString *category = [context stringNamed:@"iOS options.Category"];
 
         // Specify custom data for the notification
         NSMutableDictionary *mutableInfo;
@@ -140,11 +99,76 @@
                 mutableInfo[LP_KEY_PUSH_NO_ACTION] = messageId;
             }
         }
+        
+        if (@available(iOS 10.0, *)) {
+            UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+            
+            if (message) {
+                content.body = message;
+            } else {
+                content.body = LP_VALUE_DEFAULT_PUSH_MESSAGE;
+            }
+            
+            if (category) {
+                content.categoryIdentifier = category;
+            }
+            
+            if (sound) {
+                content.sound = [UNNotificationSound soundNamed:sound];
+            } else {
+                content.sound = [UNNotificationSound defaultSound];
+            }
 
-        localNotif.userInfo = mutableInfo;
+            if (badge) {
+                content.badge = [NSNumber numberWithInt:[badge intValue]];
+            }
 
-        // Schedule the notification
-        [app scheduleLocalNotification:localNotif];
+            content.userInfo = mutableInfo;
+            
+            NSDateComponents *dateComponenets = [[NSDateComponents alloc] init];
+            [dateComponenets setSecond:countdownSeconds];
+            UNCalendarNotificationTrigger *trigger = [UNCalendarNotificationTrigger triggerWithDateMatchingComponents:dateComponenets repeats:NO];
+            
+            UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:messageId content:content trigger:trigger];
+            
+            [[UNUserNotificationCenter currentNotificationCenter] addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+                if (error) {
+                    LPLog(LPError, error.localizedDescription);
+                }
+            }];
+            
+        } else {
+            UILocalNotification *localNotif = [[UILocalNotification alloc] init];
+            localNotif.fireDate = eta;
+            localNotif.timeZone = [NSTimeZone defaultTimeZone];
+            if (message) {
+                localNotif.alertBody = message;
+            } else {
+                localNotif.alertBody = LP_VALUE_DEFAULT_PUSH_MESSAGE;
+            }
+            localNotif.alertAction = @"View";
+
+            if ([localNotif respondsToSelector:@selector(setCategory:)]) {
+                if (category) {
+                    localNotif.category = category;
+                }
+            }
+
+            if (sound) {
+                localNotif.soundName = sound;
+            } else {
+                localNotif.soundName = UILocalNotificationDefaultSoundName;
+            }
+
+            if (badge) {
+                localNotif.applicationIconBadgeNumber = [badge intValue];
+            }
+
+            localNotif.userInfo = mutableInfo;
+
+            // Schedule the notification
+            [app scheduleLocalNotification:localNotif];
+        }
         
         if ([LPConstantsState sharedState].isDevelopmentModeEnabled) {
             LPLog(LPInfo, @"Scheduled notification");
@@ -157,20 +181,102 @@
         LP_END_USER_CODE
         UIApplication *app = [UIApplication sharedApplication];
         NSArray *notifications = [app scheduledLocalNotifications];
-        BOOL didCancel = NO;
+        if (@available(iOS 10.0, *)) {
+            __block BOOL didCancel = NO;
+            dispatch_semaphore_t semaphor = dispatch_semaphore_create(0);
+            [UNUserNotificationCenter.currentNotificationCenter getPendingNotificationRequestsWithCompletionHandler:^(NSArray<UNNotificationRequest *> * _Nonnull requests) {
+                for (UNNotificationRequest *request in requests) {
+                    NSString *messageId = [[LPNotificationsManager shared] messageIdFromUserInfo:[request.content userInfo]];
+                    if ([messageId isEqualToString:context.messageId]) {
+                        [UNUserNotificationCenter.currentNotificationCenter removeDeliveredNotificationsWithIdentifiers:@[request.identifier]];
+                        if ([LPConstantsState sharedState].isDevelopmentModeEnabled) {
+                            LPLog(LPInfo, @"Cancelled notification");
+                        }
+                        didCancel = YES;
+                    }
+                }
+                dispatch_semaphore_signal(semaphor);
+            }];
+            dispatch_time_t waitTime = dispatch_time(DISPATCH_TIME_NOW, 5.0 * NSEC_PER_SEC);
+            dispatch_semaphore_wait(semaphor, waitTime);
+            LP_BEGIN_USER_CODE
+            return didCancel;
+        } else {
+            // Fallback on earlier versions
+            BOOL didCancel = NO;
+            for (UILocalNotification *notification in notifications) {
+                NSString *messageId = [[LPNotificationsManager shared] messageIdFromUserInfo:[notification userInfo]];
+                if ([messageId isEqualToString:context.messageId]) {
+                    [app cancelLocalNotification:notification];
+                    if ([LPConstantsState sharedState].isDevelopmentModeEnabled) {
+                        LPLog(LPInfo, @"Cancelled notification");
+                    }
+                    didCancel = YES;
+                }
+            }
+            LP_BEGIN_USER_CODE
+            return didCancel;
+        }
+    }];
+}
+
+- (BOOL)shouldSendNotificationForMessage:(NSString *)message contentAvailable:(BOOL)contentAvailable
+{
+    // Don't send notification if the user doesn't have the permission enabled.
+    if ([[UIApplication sharedApplication] respondsToSelector:@selector(currentUserNotificationSettings)]) {
+        BOOL isSilentNotification = message.length == 0 && contentAvailable;
+        if (!isSilentNotification) {
+            UIUserNotificationSettings *currentSettings = [[UIApplication sharedApplication] currentUserNotificationSettings];
+            if ([currentSettings types] == UIUserNotificationTypeNone) {
+                return NO;
+            }
+        }
+    }
+    return YES;
+}
+
+- (BOOL)shouldDiscard:(NSDate *)eta context:(LPActionContext *)context
+{
+    // If there's already one scheduled before the eta, discard this.
+    // Otherwise, discard the scheduled one.
+    if (@available(iOS 10.0, *)) {
+        __block BOOL shouldDiscard = NO;
+        dispatch_semaphore_t semaphor = dispatch_semaphore_create(0);
+        [UNUserNotificationCenter.currentNotificationCenter getPendingNotificationRequestsWithCompletionHandler:^(NSArray<UNNotificationRequest *> * _Nonnull requests) {
+            for (UNNotificationRequest *request in requests) {
+                NSString *messageId = [[LPNotificationsManager shared] messageIdFromUserInfo:[request.content userInfo]];
+                if ([messageId isEqualToString:context.messageId]) {
+                    UNCalendarNotificationTrigger *trigger = (UNCalendarNotificationTrigger *)request.trigger;
+                    NSComparisonResult comparison = [trigger.nextTriggerDate compare:eta];
+                    if (comparison == NSOrderedAscending) {
+                        shouldDiscard = YES;
+                        break;
+                    } else {
+                        [UNUserNotificationCenter.currentNotificationCenter removeDeliveredNotificationsWithIdentifiers:@[request.identifier]];
+                    }
+                }
+            }
+            dispatch_semaphore_signal(semaphor);
+        }];
+        dispatch_time_t waitTime = dispatch_time(DISPATCH_TIME_NOW, 5.0 * NSEC_PER_SEC);
+        dispatch_semaphore_wait(semaphor, waitTime);
+        return shouldDiscard;
+    } else {
+        // Fallback on earlier versions
+        NSArray *notifications = [[UIApplication sharedApplication] scheduledLocalNotifications];
         for (UILocalNotification *notification in notifications) {
             NSString *messageId = [[LPNotificationsManager shared] messageIdFromUserInfo:[notification userInfo]];
             if ([messageId isEqualToString:context.messageId]) {
-                [app cancelLocalNotification:notification];
-                if ([LPConstantsState sharedState].isDevelopmentModeEnabled) {
-                    LPLog(LPInfo, @"Cancelled notification");
+                NSComparisonResult comparison = [notification.fireDate compare:eta];
+                if (comparison == NSOrderedAscending) {
+                    return YES;
+                } else {
+                    [[UIApplication sharedApplication] cancelLocalNotification:notification];
                 }
-                didCancel = YES;
             }
         }
-        LP_BEGIN_USER_CODE
-        return didCancel;
-    }];
+        return NO;
+    }
 }
 
 @end
