111a9bed20179b7e499203829982052ff501d3dd
milos1290 <milos1290@gmail.com>
2021-06-29 15:26:12 +0200

    fixing failing test

--- LeanplumSDKApp/LeanplumSDKApp/Classes/AppDelegate.swift
+++ LeanplumSDKApp/LeanplumSDKApp/Classes/AppDelegate.swift
@@ -6,14 +6,13 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UNUserNotificationCenter.current().delegate = self
         return true
     }
 
