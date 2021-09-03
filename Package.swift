d609027bdfce9ffe17b4e7e581ca60206568d010
Dejan Krstevski <dejan.krstevski@leanplum.com>
2021-09-03 10:57:57 +0200

    fix spm

--- Package.swift
+++ Package.swift
@@ -4,15 +4,20 @@ import PackageDescription
 let package = Package(
     name: "LeanplumSDK",
     products: [
-        .library(name: "LeanplumSDK", targets: ["LeanplumSDK"])
+        .library(name: "LeanplumSDK", targets: ["LeanplumSDKObjc", "LeanplumSDKSwift"])
     ],
     targets: [
         .target(
-            name: "LeanplumSDK",
-            path: "LeanplumSDK/LeanplumSDK",
-            exclude: [
-                "Supporting Files/Info.plist"
+            name: "LeanplumSDKObjc",
+            path: "LeanplumSDK/LeanplumSDK/Classes",
+            resources: [
+                .process("Resources")
             ],
+            publicHeadersPath: "include"
+        )
+        .target(
+            name: "LeanplumSDKSwift",
+            path: "LeanplumSDK/LeanplumSDK/ClassesSwift",
             resources: [
                 .process("Resources")
             ],
