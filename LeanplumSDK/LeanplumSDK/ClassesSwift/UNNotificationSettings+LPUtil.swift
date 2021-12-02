816092b4e0c6693d1246d90aea49307051997f46
Dejan Krstevski <dejan.krstevski@leanplum.com>
2021-12-02 14:17:52 +0100

    remove todo comment

--- LeanplumSDK/LeanplumSDK/ClassesSwift/UNNotificationSettings+LPUtil.swift
+++ LeanplumSDK/LeanplumSDK/ClassesSwift/UNNotificationSettings+LPUtil.swift
@@ -45,7 +45,7 @@ extension UNNotificationSettings {
         if settings.notificationCenterSetting == .enabled {
             result |= (1 << 4)
         }
-        if settings.alertStyle != .none {//settings.alertStyle == .banner || settings.alertStyle == .alert { //TODO: != .none
+        if settings.alertStyle != .none {
             result |= (1 << 5)
         }
         
