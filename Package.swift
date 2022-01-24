7d5f330b08a70830a7867af44c51504a32ef5265
leanplum-e2 <e2@leanplum.com>
2022-01-24 16:14:34 +0000

    update spm

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Leanplum",
-            url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/4.0.0-beta12/Leanplum.xcframework.zip",
+            url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/4.0.0-beta15/Leanplum.xcframework.zip",
             checksum: "d7be1fd67ac8cc0bf87bff2907e1e51e3ae3fe40633b1bf768bc8df6080d3bd1"
         ),
        .target(
