57d099a3305afa1ab213cb65134b2b63a1906371
dejan2k <dejan.krstevski@leanplum.com>
2022-03-16 14:06:10 +0100

    add support in WKWebView for urls with target="_blank" (#490)
    * add support in WKWebView for urls with target="_blank"
    
    * re-indent

--- LeanplumSDK/LeanplumSDK/Classes/MessageTemplates/ViewControllers/LPWebInterstitialViewController.h
+++ LeanplumSDK/LeanplumSDK/Classes/MessageTemplates/ViewControllers/LPWebInterstitialViewController.h
@@ -13,7 +13,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface LPWebInterstitialViewController : UIViewController <WKNavigationDelegate>
+@interface LPWebInterstitialViewController : UIViewController <WKNavigationDelegate, WKUIDelegate>
 
 @property (strong, nonatomic) LPActionContext *context;
 
