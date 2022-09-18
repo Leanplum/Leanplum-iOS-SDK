f15ab69c2cdcdd57dda90c18956011d709c8edd1
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2022-09-19 00:29:55 +0300

    Update SPM

--- Package.swift
+++ Package.swift
@@ -3,20 +3,32 @@ import PackageDescription
 
 let package = Package(
     name: "Leanplum",
+    platforms: [
+        .iOS(.v9)
+    ],
     products: [
-        .library(name: "Leanplum", targets: ["Leanplum"]),
+        .library(name: "Leanplum", targets: ["LeanplumTargetWrapper"]),
         .library(name: "LeanplumLocation", targets: ["LeanplumLocation"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/CleverTap/clevertap-ios-sdk", from: "4.1.1")
     ],
     targets: [
         .binaryTarget(
             name: "Leanplum",
+            //path: "Release/static/Leanplum.xcframework"
             url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/4.1.0/Leanplum.xcframework.zip",
-            checksum: "598e45b6118697ec2252fbe249dba2764ca1e094b8434818e6abeb24635fd0c8"
+            checksum: "8babd16ec7ebfc3f939bfe312ead4c3b16e4b2e3de5996cd920953ab1f725524"
         ),
-       .target(
-           name: "LeanplumLocation",
-           path: "LeanplumSDKLocation",
-           publicHeadersPath: "LeanplumSDKLocation/include"
-       )
+        .target(
+            name: "LeanplumLocation",
+            dependencies: ["Leanplum"],
+            path: "LeanplumSDKLocation",
+            publicHeadersPath: "LeanplumSDKLocation/include"
+        ),
+        .target(name: "LeanplumTargetWrapper",
+                dependencies: ["Leanplum", .product(name: "CleverTapSDK", package: "clevertap-ios-sdk")],
+                path: "LeanplumTargetWrapper"
+               )
     ]
 )
