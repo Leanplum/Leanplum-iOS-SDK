e06bb35d5aaebb06a680d27f8f0f9edb978414d9
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2022-10-03 12:23:31 +0300

    Remove module name from xcframework swiftinterface (#516)
    * swiftinterface remove module name from static
    
    * Specify simulator destination

--- Tools/build.sh
+++ Tools/build.sh
@@ -113,7 +113,7 @@ build_ios_dylib() {
   -scheme $scheme \
   -archivePath $archivePath-iphonesimulator.xcarchive \
   -sdk iphonesimulator \
-  -destination generic/platform=iOS \
+  -destination 'generic/platform=iOS Simulator' \
   SKIP_INSTALL=NO
 
   echo "Building $scheme dynamic (device) target ..."
@@ -186,7 +186,7 @@ build_ios_static() {
   -scheme $scheme \
   -archivePath $archivePath-iphonesimulator.xcarchive \
   -sdk iphonesimulator \
-  -destination generic/platform=iOS \
+  -destination 'generic/platform=iOS Simulator' \
   SKIP_INSTALL=NO
 
   echo "Building $scheme static (device) target ..."
@@ -203,6 +203,12 @@ build_ios_static() {
   -framework $archivePath-iphonesimulator.xcarchive/Products/Library/Frameworks/$productName.framework \
   -framework $archivePath-iphoneos.xcarchive/Products/Library/Frameworks/$productName.framework \
   -output Release/static/$productName.xcframework
+  
+  # Remove module name from xcframework swiftinterface
+  # It prevents error X is not a member of type Leanplum.Leanplum
+  # This happens when a class name is same as the module name
+  # https://stackoverflow.com/a/62310245
+  find Release/static/Leanplum.xcframework -name "*.swiftinterface" -exec sed -i -e "s/Leanplum\.//g" {} \;
 
   echo "Removing arm64 from simulator slice ..."
   lipo -remove arm64 \
