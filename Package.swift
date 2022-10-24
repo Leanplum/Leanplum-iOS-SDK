3bc40d899138982c273b55126aaf5ba41cfa9286
leanplum-e2 <e2@leanplum.com>
2022-10-24 19:40:46 +0000

    update spm

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ let package = Package(
         .library(name: "LeanplumLocation", targets: ["LeanplumLocation"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/6.0.0-beta6/Leanplum.xcframework.zip",
+        .package(url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/6.0.0-beta7/Leanplum.xcframework.zip",
     ],
     targets: [
         .binaryTarget(
             name: "Leanplum",
             url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/5.0.0/Leanplum.xcframework.zip",
-            checksum: "d813b6669f0c30bd5661c5f3841517e887d138d5b3bf04e5760e6ba0bee93265"
+            checksum: "d6a9f616125d1ced1e4ea9b1fb326eb292db329cb46fe84f54c8020ff92caf1b"
         ),
         .target(
             name: "LeanplumLocation",
