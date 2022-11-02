122642914f07b9684019b2a806a65407f87fe585
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2022-11-02 20:02:11 +0200

    Fix spm package

--- Package.swift
+++ Package.swift
@@ -11,12 +11,12 @@ let package = Package(
         .library(name: "LeanplumLocation", targets: ["LeanplumLocation"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/6.0.1/Leanplum.xcframework.zip",
+        .package(url: "https://github.com/CleverTap/clevertap-ios-sdk", from: "4.1.4")
     ],
     targets: [
         .binaryTarget(
             name: "Leanplum",
-            url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/5.0.0/Leanplum.xcframework.zip",
+            url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/6.0.1/Leanplum.xcframework.zip",
             checksum: "8bd35c71da15bdb14facbcc3b09d7757695d0a9639336bf7b9097849c614d058"
         ),
         .target(
