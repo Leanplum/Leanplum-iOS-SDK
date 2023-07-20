10695591488e2f129cccee8ac0c80da75ced1699
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2023-07-20 20:16:10 +0300

    Release/6.1.1 (#570)
    * update version
    
    * Update SPM

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Leanplum",
-            url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/6.1.0/Leanplum.xcframework.zip",
-            checksum: "791978f8c3e50cd4ebd1e8538424e2699347b4fa77a87f1c90c5e141ddfd2cf2"
+            url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/6.1.1/Leanplum.xcframework.zip",
+            checksum: "66c1a3f7becb2a8ecc705bf25eb521c36aa0098937304fa86c1fdc7bbd303de3"
         ),
         .target(
             name: "LeanplumLocation",
