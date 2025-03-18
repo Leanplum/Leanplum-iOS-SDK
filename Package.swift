0a1f2142bab6db644bb96b7b474e42c1285b1001
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2025-03-18 13:45:35 +0200

    Release/6.6.1 (#600)
    * update version

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ let package = Package(
         .library(name: "LeanplumLocation", targets: ["LeanplumLocation"])
     ],
     dependencies: [
-        .package(url: "https://github.com/CleverTap/clevertap-ios-sdk", from: "7.1.0")
+        .package(url: "https://github.com/CleverTap/clevertap-ios-sdk", from: "7.1.1")
     ],
     targets: [
         .binaryTarget(
             name: "Leanplum",
-            url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/6.6.0/Leanplum.xcframework.zip",
-            checksum: "95488853cd21913714c46e3debb9dfb8e3b9fba5968374c08664459ea038e8ff"
+            url: "https://github.com/Leanplum/Leanplum-iOS-SDK/releases/download/6.6.1/Leanplum.xcframework.zip",
+            checksum: "47e4d86508c83255241219f89441ea007b3683fc2569384d2928b6b45e91db01"
         ),
         .target(
             name: "LeanplumLocation",
